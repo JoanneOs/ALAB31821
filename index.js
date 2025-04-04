e1e0be80559fead24b9382fd4e0985d2829440b2
JoanneOs <jowan.awsman@yahoo.com>
2025-04-04 14:22:47 -0500

    Initial commit for ExpressApp

---
+++ index.js
@@ -0,0 +1,10 @@
+//greated folder then file index.js
+//npm init -y
+//sudo npm install express ejs
+
+// I push it wrong repo:
+// git remote remove origin
+// git remote add origin https://github.com/JoanneOs/ALAB31821.git
+// git branch -M main
+// git push -u origin main
+// now my code will go to the right repo.
